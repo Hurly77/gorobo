5b4723d1b9a262068b39d453d061a52550d7b06d
C.J Levey <cameron.leverett@yahoo.com>
2020-05-27 11:22:32 -0700

    Builder = user moved Login/out/up to builder

--- gorobo/app/controllers/application_controller.rb
+++ gorobo/app/controllers/application_controller.rb
@@ -8,55 +8,7 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "no_peeping_jerrys"
   end
-
-  get '/signup' do
-    if !logged_in?
-
-      erb :'/builders/new.html'
-     else
-      redirect to '/builders'
-    end
-  end
-
-  post '/signup' do
-    @builder = Builder.new(name: params[:name], user_name: params[:user_name], password: params[:password], email: params[:email]) #need more info on password_confirmation
-    if @builder.save
-      session[:builder_id] = @builder.id
-
-      redirect to "/builders/#{@builder.id}"
-    else 
-      redirect to '/'
-    end
-    
-  end
-
-  get '/login' do
-    if !logged_in?
-
-      erb:'builders/index.html'
-     else
-      redirect to'/builders'
-    end
-  end
-
-  post '/login' do
-    @builder = Builder.find_by(user_name: params[:user_name])
-    if @builder && @builder.authenticate(params[:password])
-      session[:builder_id] = @builder.id
-
-      redirect to "/builders/#{@builder.id}"
-     else
-      redirect to '/'
-    end
-  end
-
-  get '/logout' do
-    if logged_in?
-      session.clear
-      redirect to '/'
-    end
-  end
-
+  
   get "/" do
     erb :index
   end
@@ -70,11 +22,5 @@ class ApplicationController < Sinatra::Base
     def current_builder
       Builder.find(session[:builder_id])
     end
-
-    def except!(keys)
-      keys.each { |key| delete(key) }
-      self
-    end
-
   end
 end
--- gorobo/app/controllers/builders_controller.rb
+++ gorobo/app/controllers/builders_controller.rb
@@ -1,20 +1,56 @@
 class BuildersController < ApplicationController
 
-  # GET: /builders
+  get '/signup' do
+    if !logged_in?
+
+      erb :'/builders/new.html'
+     else
+      redirect to '/builders'
+    end
+  end
+
+  post '/signup' do
+    @builder = Builder.new(name: params[:name], user_name: params[:user_name], password: params[:password], email: params[:email]) #need more info on password_confirmation
+    if @builder.save
+      session[:builder_id] = @builder.id
+
+      redirect to "/builders/#{@builder.id}"
+    else 
+      redirect to '/'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+
+      erb:'builders/index.html'
+     else
+      redirect to'/builders'
+    end
+  end
+
+  post '/login' do
+    @builder = Builder.find_by(user_name: params[:user_name])
+    if @builder && @builder.authenticate(params[:password])
+      session[:builder_id] = @builder.id
+
+      redirect to "/builders/#{@builder.id}"
+     else
+      redirect to '/'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/'
+    end
+  end
+
   get "/builders" do
     @builder = Builder.find_by(params[:builder_id])
 
     redirect to "/builders/#{@builder.id}"
-  end
-
-  # POST: /builders
-  post "/builders" do
-    redirect "/builders/:id"
-  end
-
-  # GET: /builders/new
-  get "/builders/new" do
-    erb :"/builders/new.html"
   end
 
   # GET: /builders/5
@@ -40,11 +76,14 @@ class BuildersController < ApplicationController
     @builder.email
     @builder.password
     @builder.save
-    redirect "/builders/:id"
+    redirect "/builders/#{@builder.id}"
   end
 
   # DELETE: /builders/5/delete
   delete "/builders/:id/delete" do
-    redirect "/builders"
+    
+    current_builder.destroy
+    session.clear
+    redirect "/"
   end
 end
